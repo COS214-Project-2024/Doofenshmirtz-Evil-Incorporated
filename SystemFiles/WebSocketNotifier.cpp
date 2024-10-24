2cf0533a576a9c2d9dfac7494c6ffd0b6d8f4479
Tiaan Bosman <bosmantiaan22@gmail.com>
2024-10-24 16:07:08 +0200

    Added doxygen

--- SystemFiles/WebSocketNotifier.cpp
+++ SystemFiles/WebSocketNotifier.cpp
@@ -1,10 +1,14 @@
 #include "WebSocketNotifier.h"
 
-WebSocketNotifier::WebSocketNotifier() : myServer(nullptr)
-{
-	// Constructor, assign null to server
-}
-
+/**
+ * @brief Log a message by sending it to the WebSocket server.
+ *
+ * This function delegates the message to the server's write function
+ * if the server instance has been set. If not, it outputs an error
+ * message to the console.
+ *
+ * @param message The JSON object to be sent to the server.
+ */
 void WebSocketNotifier::log(nlohmann::json message)
 {
 	if (myServer)
@@ -13,24 +17,19 @@ void WebSocketNotifier::log(nlohmann::json message)
 	}
 	else
 	{
-		std::cout << "Server not set for WebSocketNotifier" << std::endl; // No server
+		std::cout << "Server not set for WebSocketNotifier" << std::endl;
 	}
 }
 
+/**
+ * @brief Set the server instance for this notifier.
+ *
+ * This function allows the WebSocketNotifier to reference a specific
+ * Server instance to which it can send log messages.
+ *
+ * @param server Pointer to the Server instance to set.
+ */
 void WebSocketNotifier::setServer(Server *server)
 {
-	myServer = server; // Sets server
-}
-
-WebSocketNotifier *WebSocketNotifier::getInstance()
-{
-	if (Instance == nullptr) // If no instance
-	{
-		Instance = new WebSocketNotifier(); // create instance
-	}
-	return Instance;
-}
-
-WebSocketNotifier::~WebSocketNotifier()
-{
+	myServer = server;
 }
--- SystemFiles/WebSocketNotifier.h
+++ SystemFiles/WebSocketNotifier.h
@@ -1,24 +1,29 @@
 #ifndef WEBSOCKETNOTIFIER_H
 #define WEBSOCKETNOTIFIER_H
-
 #include "Server.h"
-#include <iostream>
+#include <boost/serialization/singleton.hpp>
+#include <nlohmann/json.hpp>
 
-class WebSocketNotifier
+/**
+ * @brief Singleton class for logging messages via WebSocket.
+ *
+ * This class serves as a notifier that sends log messages
+ * to the connected WebSocket server. It is designed as a
+ * singleton to ensure a single instance is used throughout
+ * the application.
+ */
+class WebSocketNotifier : public boost::serialization::singleton<WebSocketNotifier>
 {
-
 public:
-	static WebSocketNotifier *getInstance();
+	/// Sends a JSON message to the WebSocket server.
+	/// @param message The JSON object containing the message to log.
 	void log(nlohmann::json message);
+	/// Sets the server instance for this notifier.
+	/// @param server Pointer to the Server instance to send messages.
 	void setServer(Server *server);
 
-protected:
-	WebSocketNotifier();
-	~WebSocketNotifier();
-
 private:
-	Server *myServer;
-	static WebSocketNotifier *Instance;
+	Server *myServer; ///< Pointer to the WebSocket server instance.
 };
 
 #endif
