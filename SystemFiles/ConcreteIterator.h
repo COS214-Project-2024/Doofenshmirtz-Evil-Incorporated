877edf513a6e044013d314e688929e9c5e6dcb61
Jack-jack1 <jackbawden01@gmail.com>
2024-10-26 12:47:16 +0200

    added necessary attributes

--- SystemFiles/ConcreteIterator.h
+++ SystemFiles/ConcreteIterator.h
@@ -2,22 +2,26 @@
 #define CONCRETEITERATOR_H
 
 #include "Iterator.h"
+#include "CityUnit.h"
 
 class ConcreteIterator : public Iterator {
-
+private:
+	const std::vector<CityUnit*>& cityUnits;
+	size_t index;
 
 public:
-	ConcreteIterator();
+	ConcreteIterator(const std::vector<CityUnit*>& cityUnitsParam);
 
 	~ConcreteIterator();
 
 	void first();
 
 	void next();
+	void previous();
 
-	void isDone();
+	bool isDone();
 
-	void currentItem();
+	CityUnit* currentItem();
 };
 
 #endif
