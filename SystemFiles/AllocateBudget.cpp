5768b1f4b257c01cb8a966b6fdb4497c54a781b2
NicohaasHerhaasus <hencopritor06@gmail.com>
2024-10-31 21:00:51 +0200

    Implemented AllocateBudget, Some super complex stuff you should go check.

--- SystemFiles/AllocateBudget.cpp
+++ SystemFiles/AllocateBudget.cpp
@@ -1,8 +1,7 @@
 #include "AllocateBudget.h"
 
 AllocateBudget::AllocateBudget() {
-	// TODO - implement AllocateBudget::AllocateBudget
-	throw "Not yet implemented";
+
 }
 
 AllocateBudget::~AllocateBudget()
@@ -10,6 +9,5 @@ AllocateBudget::~AllocateBudget()
 }
 
 void AllocateBudget::excecuteCommand() {
-	// TODO - implement AllocateBudget::excecuteCommand
-	throw "Not yet implemented";
+	//budget.allocate();
 }
