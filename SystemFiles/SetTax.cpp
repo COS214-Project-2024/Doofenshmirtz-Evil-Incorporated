770e630772302564c7f19ba69e08d3574ee90998
NicohaasHerhaasus <hencopritor06@gmail.com>
2024-11-02 18:57:13 +0200

    Added dox comments to SetTax concrete command

--- SystemFiles/SetTax.cpp
+++ SystemFiles/SetTax.cpp
@@ -1,14 +1,26 @@
+/**
+ * @file SetTax.cpp
+ * @brief Implements the SetTax class, which sets the tax rate for a CityUnit.
+ */
+
 #include "SetTax.h"
 
-SetTax::SetTax(CityUnit* district,double newTaxRatePercentage) : GovernmentCommand(district)
-{
-	this->taxRate = newTaxRatePercentage;
+/**
+ * @brief Constructs a SetTax command with the specified tax rate.
+ */
+SetTax::SetTax(CityUnit* district, double newTaxRatePercentage) : GovernmentCommand(district) {
+    this->taxRate = newTaxRatePercentage;
 }
 
-SetTax::~SetTax()
-{
+/**
+ * @brief Destructor for SetTax.
+ */
+SetTax::~SetTax() {
 }
 
+/**
+ * @brief Executes the command to set the tax rate for the associated CityUnit.
+ */
 void SetTax::executeCommand() {
-	this->reciever->setTaxRate(taxRate);
+    this->reciever->setTaxRate(taxRate);
 }
--- SystemFiles/SetTax.h
+++ SystemFiles/SetTax.h
@@ -1,18 +1,42 @@
+/**
+ * @file SetTax.h
+ * @brief Declares the SetTax class, which is responsible for setting the tax rate in a CityUnit.
+ */
+
 #ifndef SETTAX_H
 #define SETTAX_H
 
-
 #include "GovernmentCommand.h"
 #include "CityUnit.h"
+
+/**
+ * @class SetTax
+ * @brief Command to set the tax rate for a specific CityUnit.
+ *
+ * This command encapsulates the action of changing the tax rate for the associated CityUnit,
+ * which can influence its financial state and operations.
+ */
 class SetTax : public GovernmentCommand {
 private:
-	double taxRate;
+    double taxRate; ///< The new tax rate percentage to be applied.
+
 public:
-	SetTax(CityUnit* district,double newTaxRatePercentage);
+    /**
+     * @brief Constructs a SetTax command with a specified tax rate.
+     * @param district Pointer to the CityUnit where the tax rate will be set.
+     * @param newTaxRatePercentage The new tax rate percentage to be applied.
+     */
+    SetTax(CityUnit* district, double newTaxRatePercentage);
 
-	~SetTax();
+    /**
+     * @brief Destructor for SetTax.
+     */
+    ~SetTax();
 
-	void executeCommand();
+    /**
+     * @brief Executes the command by applying the new tax rate to the CityUnit.
+     */
+    void executeCommand();
 };
 
 #endif
