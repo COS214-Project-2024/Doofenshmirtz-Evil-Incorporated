96b09e41094ae62c4c6095b5fb57fc12b80e709a
Johna <johnpeterkrause@gmail.com>
2024-11-03 19:55:07 +0200

    Corrected Dynamic cast

--- SystemFiles/Building.cpp
+++ SystemFiles/Building.cpp
@@ -44,7 +44,7 @@ int Building::getTotalCapacity() const {
 double Building::getEmploymentRate() {
 
     // Check if this building is a Residential type
-    Building* residentialBuilding = dynamic_cast<Residential*>(this);
+    Residential* residentialBuilding = dynamic_cast<Residential*>(this);
     if (!residentialBuilding) {
         // If not residential, return 0 as employment rate
         return 0.0;
